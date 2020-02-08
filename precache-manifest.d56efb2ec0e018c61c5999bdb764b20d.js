dbcb3739d7a6229d4fdd86a0ffca7fb1167e9368
Hariom Singh <iamhariomsingh@gmail.com>
2020-02-08 16:01:58 +0530

    Updates

---
+++ precache-manifest.d56efb2ec0e018c61c5999bdb764b20d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f8c6fb81ddce372c17d70736699581",
+    "url": "/myprojects/index.html"
+  },
+  {
+    "revision": "43e8cc0766de6cfc2ca2",
+    "url": "/myprojects/static/js/2.727a4bdc.chunk.js"
+  },
+  {
+    "revision": "94b03e19a4c9eb66db847f3cca1e03ba",
+    "url": "/myprojects/static/js/2.727a4bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f746640cb54388f0c3",
+    "url": "/myprojects/static/js/main.2bd2886c.chunk.js"
+  },
+  {
+    "revision": "b4f780359e21cf52469b",
+    "url": "/myprojects/static/js/runtime-main.6c42d4b5.js"
+  }
+]);
